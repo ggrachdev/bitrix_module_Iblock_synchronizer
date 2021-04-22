456b84645459c2ead0a3aeb396c6434f546a4152
ggrachdev <ggrachdev@yandex.ru>
2021-04-22 16:19:48 +0400

    Add exceptions

--- ggrachdev.iblock_synchronizer/classes/general/IblockSynchronizer/Synchronizer.php
+++ ggrachdev.iblock_synchronizer/classes/general/IblockSynchronizer/Synchronizer.php
@@ -3,5 +3,13 @@
 namespace GGrach\IblockSynchronizer;
 
 class Synchronizer {
-    //put your code here
+    
+    /**
+     * @param int $fromIblockId - Откуда брать данные
+     * @param int $toIblockId - Куда синхронизировать данные
+     */
+    public function __construct(int $fromIblockId, int $toIblockId) {
+        
+    }
+
 }
--- ggrachdev.iblock_synchronizer/include.php
+++ ggrachdev.iblock_synchronizer/include.php
@@ -1,5 +1,9 @@
 <?
 \Bitrix\Main\Loader::registerAutoLoadClasses('ggrachdev.iblock_synchronizer', [
-    "\GGrach\IblockSynchronizer\Synchronizer" => "classes/general/IblockSynchronizer/Synchronizer.php"
+    "\GGrach\IblockSynchronizer\Synchronizer" => "classes/general/IblockSynchronizer/Synchronizer.php",
+    
+    // exceptions
+    "\GGrach\IblockSynchronizer\Exceptions\BitrixRedactionException" => "classes/general/IblockSynchronizer/Exceptions/BitrixRedactionException.php",
+    "\GGrach\IblockSynchronizer\Exceptions\SearchIblockException" => "classes/general/IblockSynchronizer/Exceptions/SearchIblockException.php",
 ]);
 ?>
