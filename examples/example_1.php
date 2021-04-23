de93b0e9ec4ca5a1a961ddc169f747ab64c91c4a
ggrachdev <ggrachdev@yandex.ru>
2021-04-23 12:49:11 +0400

    Fix example

--- examples/example_1.php
+++ examples/example_1.php
@@ -12,11 +12,15 @@ if (\Bitrix\Main\Loader::includeModule('ggrachdev.iblock_synchronizer')) {
     $synchronizerWrapper->setSyncRules([
         // Задаем похожие свойства по которым искать соответствия
         'SIMILAR_PROPERTIES' => [
-            'XML_ID', 'PROPERTY_CML2_ARTICLE'
+            'PROPERTY_CML2_ARTICLE'
         ],
         // Задаем свойства, которые нужно синхронизировать
         'SYNC_PROPERTIES' => [
             'PRICE'
+        ],
+        // Соответствия свойств инфоблок from => инфоблок to
+        'CONFORMITY' => [
+            'PROPERTY_CML2_ARTICLE' => 'PROPERTY_VENDOR_CODE'
         ]
     ]);
 
@@ -27,5 +31,7 @@ if (\Bitrix\Main\Loader::includeModule('ggrachdev.iblock_synchronizer')) {
     print_r($syncResult->getSimilarIds());
     print_r($syncResult->getSynchronizedIds());
     print_r($syncResult->getNotSynchronizedIds());
+    print_r($syncResult->getSynchronizedData());
+    var_dump($syncResult->isSuccess());
     echo '</pre>';
 }
